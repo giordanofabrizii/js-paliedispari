dfd0630d1572a7d6536cbf613b86fed2ce4b264d
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-16 18:56:02 +0200

    added pari e dispari script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // > PALINDROMA
-let stringa = prompt("scrivi una parola").toLowerCase()// inizializzo una variabile con prompt
+// let stringa = prompt("scrivi una parola").toLowerCase()
 
 function palindroma(str) {
     let reverted = "";
@@ -7,16 +7,34 @@ function palindroma(str) {
         reverted += stringa[i];
     }
     if (stringa === reverted) {
-        console.log("la parola e' palindroma")
+        console.log("la parola e' palindroma");
     } else {
-        console.log("la parola non e' palindroma")
+        console.log("la parola non e' palindroma");
     }
 }
 
-palindroma(stringa);
-//     inizializzo una variabile che rappresenta la stringa girata
-//     creo un ciclo for dalla length della stringa a 0
-//         stringaGirata + stringa[index]
-//     se sono uguali
-//         return true
-//     return false
+// palindroma(stringa);
+
+
+// > PARI E DISPARI
+let moneta = prompt("ciao scegli tra pari e dispari").toLowerCase(); // sceglie tra pari e dispari
+while (moneta !== "pari" && moneta !== "dispari") {
+    moneta = prompt("ciao scegli tra pari e dispari").toLowerCase();
+}
+
+let numero = Number.parseInt(prompt("scrivi un numero da 1 a 5")); // chiedo un numero da 1 a 5
+while (isNaN(numero) != 0 || numero > 5 || numero < 1) {
+    numero = Number.parseInt(prompt("scrivi un numero da 1 a 5"));
+}
+
+function pariDispari(scelta, number) {
+    let somma = number + Math.floor(Math.random()*5 + 1); // sommo moneta e un numero da 1 a 5
+
+    if ((somma % 2 == 0 && scelta == "pari") || (somma % 2 == 1 && scelta == "dispari")) {
+        console.log("hai vinto");
+    } else {
+        console.log("hai perso")
+    }
+}
+
+pariDispari(moneta, numero);
